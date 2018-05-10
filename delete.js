544fcc1a0cb67a0f61e83c221041a4e523e349ba
Travis Dahlin <trdahlin@gmail.com>
2018-05-09 19:54:54 -0700

    delete-note correct typo in import dynamodb-lib

--- delete.js
+++ delete.js
@@ -1,4 +1,4 @@
-import * as dynamoDbLib from "./libs/dynabodb-lib"
+import * as dynamoDbLib from "./libs/dynamodb-lib"
 import { success, failure } from "./libs/response-lib"
 
 export async function main(event, context, callback) {
