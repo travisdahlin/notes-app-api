697216ce119e694759a83bb681b2c3683992a032
Travis Dahlin <trdahlin@gmail.com>
2018-08-27 22:18:20 -0700

    Add ddb resource

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,8 +1,8 @@
-import AWS from 'aws-sdk'
+import AWS from "aws-sdk";
 
-AWS.config.update({ region: "us-west-2" })
+// AWS.config.update({ region: "us-west-2" })
 
 export function call(action, params) {
-	const dynamoDb = new AWS.DynamoDB.DocumentClient()
-	return dynamoDb[action](params).promise()
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  return dynamoDb[action](params).promise();
 }
